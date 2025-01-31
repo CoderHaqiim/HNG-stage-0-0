fca2294ed020c2e6e8b14adcd36f4249692a06c6
Haqiim <hqcoder.jh@gmail.com>
2025-01-31 13:09:04 +0100

    #

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-const http = require('http')
-const path = require('path')
-const PORT = "8080"
-const dateTime = new Date().toISOString()
+const http = require('http');
+const PORT = 8080;
 
-const server = http.createServer((req, res)=>{
-    res.writeHead(200, {
-        'Content-type': "application/json"
-    })
-    
-    if(req.method === 'GET' && res.url== "/details"){
-        res.write(200,{
-            "content-type":"application/josn"
-        })
+const server = http.createServer((req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        return res.end();
+    }
+
+    if (req.method === 'GET' && req.url === "/api/details") {
+        res.writeHead(200, {
+            'Content-Type': "application/json"
+        });
+
         res.write(JSON.stringify({
             email: "jaybiddy.jh@gmail.com",
-            current_datetime: dateTime,
-            github_url: fff
-        }))
-    }
-    res.end()
-})
+            current_datetime: new Date().toISOString(), 
+            github_url: "https://github.com/CoderHaqiim/HNG-stage-0-0"
+        }));
 
-server.listen(PORT, ()=>{
-    console.log("server listening at port" + PORT)
-})
+        res.end()
+    }else{
+        res.writeHead(404, {
+            'Content-Type': "application/json"
+        });
+        res.end(JSON.stringify({message: "Requested resoource not found"}))
+    }
+    
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening at port ${ PORT}`);
+});
